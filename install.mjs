82524474fb04e70f5b1cf69d981ca7736a9d1f3f
nshen <nshen121@gmail.com>
2022-11-06 15:42:04 +0800

    fix install.mjs clone branch

--- install.mjs
+++ install.mjs
@@ -86,7 +86,7 @@ blue("");
 blue("开始下载全新配置，请稍后...");
 
 // red(await $`pwd`);
-await $`git clone --branch v2 https://github.com/nshen/learn-neovim-lua ${nvimDir}`;
+await $`git clone --depth 1 https://github.com/nshen/insisVim ${nvimDir}`;
 
 blue("");
 green("下载成功");
