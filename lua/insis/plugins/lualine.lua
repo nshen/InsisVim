07c13f107d89a9f2e884dcc1d9f83e1c981896b1
nshen <nshen121@gmail.com>
2022-11-08 13:27:49 +0800

    display relative path in lualine

--- lua/insis/plugins/lualine.lua
+++ lua/insis/plugins/lualine.lua
@@ -21,14 +21,13 @@ if lualine then
     },
     extensions = { "nvim-tree" },
     sections = {
-      -- lualine_c = {
-      --   "filename",
-      --   {
-      -----  arkav/lualine-lsp-progress
-      --     "lsp_progress",
-      --     spinner_symbols = { " ", " ", " ", " ", " ", " " },
-      --   },
-      -- },
+      lualine_c = {
+        {
+          "filename",
+          file_status = true, -- displays file status (readonly status, modified status)
+          path = 1, -- 0 = just filename, 1 = relative path, 2 = absolute path
+        },
+      },
       lualine_x = {
         "filesize",
         {
