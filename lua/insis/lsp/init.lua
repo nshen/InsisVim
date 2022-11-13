8fead6b0cc6a04909a904ba7b9fc648533f30078
nshen <nshen121@gmail.com>
2022-11-14 01:33:22 +0800

    refactor lsp

--- lua/insis/lsp/init.lua
+++ lua/insis/lsp/init.lua
@@ -1,18 +1,8 @@
-local status, mason = pcall(require, "mason")
-if not status then
-  vim.notify("没有找到 mason")
-  return
-end
+local mason = pRequire("mason")
+local mason_config = pRequire("mason-lspconfig")
+local lspconfig = pRequire("lspconfig")
 
-local status, mason_config = pcall(require, "mason-lspconfig")
-if not status then
-  vim.notify("没有找到 mason-lspconfig")
-  return
-end
-
-local status, lspconfig = pcall(require, "lspconfig")
-if not status then
-  vim.notify("没有找到 lspconfig")
+if not mason or not mason_config or not lspconfig then
   return
 end
 
@@ -29,55 +19,39 @@ mason.setup({
   },
 })
 
+local ensure_installed, servers = require("insis.utils.config-helper").getMasonEnsureList()
 -- mason-lspconfig uses the `lspconfig` server names in the APIs it exposes - not `mason.nvim` package names
 -- https://github.com/williamboman/mason-lspconfig.nvim/blob/main/doc/server-mapping.md
 mason_config.setup({
-  ensure_installed = {
-    "sumneko_lua",
-    "tsserver",
-    "tailwindcss",
-    "bashls",
-    "cssls",
-    "dockerls",
-    "emmet_ls",
-    "html",
-    "jsonls",
-    "pyright",
-    "rust_analyzer",
-    "taplo",
-    "yamlls",
-    "gopls",
-    "clangd",
-    "cmake",
-  },
+  ensure_installed = ensure_installed,
 })
 
 -- 安装列表
 -- { key: 服务器名， value: 配置文件 }
 -- key 必须为下列网址列出的 server name，不可以随便写
 -- https://github.com/williamboman/nvim-lsp-installer#available-lsps
-local servers = {
-  sumneko_lua = require("insis.lsp.config.lua"), -- lua/lsp/config/lua.lua
-  bashls = require("insis.lsp.config.bash"),
-  -- pyright = require("insis.lsp.config.pyright"),
-  pylsp = require("insis.lsp.config.pylsp"),
-  html = require("insis.lsp.config.html"),
-  cssls = require("insis.lsp.config.css"),
-  emmet_ls = require("insis.lsp.config.emmet"),
-  jsonls = require("insis.lsp.config.json"),
-  -- DEPRECATE
-  -- tsserver = require("insis.lsp.config.ts"),
-  tsserver = require("insis.lsp.config.typescript"),
-  yamlls = require("insis.lsp.config.yamlls"),
-  dockerls = require("insis.lsp.config.docker"),
-  tailwindcss = require("insis.lsp.config.tailwindcss"),
-  rust_analyzer = require("insis.lsp.config.rust"),
-  taplo = require("insis.lsp.config.taplo"), -- toml
-  gopls = require("insis.lsp.config.gopls"),
-  -- remark_ls = require("insis.lsp.config.markdown"),
-  clangd = require("insis.lsp.config.clangd"),
-  cmake = require("insis.lsp.config.cmake"),
-}
+-- local servers = {
+--   sumneko_lua = require("insis.lsp.config.lua"), -- lua/lsp/config/lua.lua
+--   bashls = require("insis.lsp.config.bash"),
+--   -- pyright = require("insis.lsp.config.pyright"),
+--   pylsp = require("insis.lsp.config.pylsp"),
+--   html = require("insis.lsp.config.html"),
+--   cssls = require("insis.lsp.config.css"),
+--   emmet_ls = require("insis.lsp.config.emmet"),
+--   jsonls = require("insis.lsp.config.json"),
+--   -- DEPRECATE
+--   -- tsserver = require("insis.lsp.config.ts"),
+--   tsserver = require("insis.lsp.config.typescript"),
+--   yamlls = require("insis.lsp.config.yamlls"),
+--   dockerls = require("insis.lsp.config.docker"),
+--   tailwindcss = require("insis.lsp.config.tailwindcss"),
+--   rust_analyzer = require("insis.lsp.config.rust"),
+--   taplo = require("insis.lsp.config.taplo"), -- toml
+--   gopls = require("insis.lsp.config.gopls"),
+--   -- remark_ls = require("insis.lsp.config.markdown"),
+--   clangd = require("insis.lsp.config.clangd"),
+--   cmake = require("insis.lsp.config.cmake"),
+-- }
 
 for name, config in pairs(servers) do
   if config ~= nil and type(config) == "table" then
