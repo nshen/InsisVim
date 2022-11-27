dff90396934beb4dc961b6a8fb7c46a66e4cfcf2
nshen <nshen121@gmail.com>
2022-11-27 12:46:13 +0800

    update gitsigns

--- lua/insis/plugins/gitsigns.lua
+++ lua/insis/plugins/gitsigns.lua
@@ -63,7 +63,7 @@ if gitsigns and cfg and cfg.enable then
     --  C for change
     --  D for delete
     signs = {
-      add = { hl = "GitSignsAdd", text = "a|", numhl = "GitSignsAddNr", linehl = "GitSignsAddLn" },
+      add = { hl = "GitSignsAdd", text = "+|", numhl = "GitSignsAddNr", linehl = "GitSignsAddLn" },
       change = { hl = "GitSignsChange", text = "c|", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
       delete = { hl = "GitSignsDelete", text = "d_", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
       topdelete = { hl = "GitSignsDelete", text = "d‾", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
