e2235365e7bfa5c34d43e3c940f434c952128b86
Xin Chen <nshen121@gmail.com>
2023-04-03 02:37:22 +0800

    fix: lua lsp config

--- lua/insis/lsp/config/lua.lua
+++ lua/insis/lsp/config/lua.lua
@@ -6,6 +6,28 @@ local opts = {
     common.disableFormat(client)
     common.keyAttach(bufnr)
   end,
+  -- https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#lua_ls
+  settings = {
+    Lua = {
+      runtime = {
+        -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+        version = "LuaJIT",
+      },
+      diagnostics = {
+        -- Get the language server to recognize the `vim` global
+        globals = { "vim" },
+      },
+      workspace = {
+        -- Make the server aware of Neovim runtime files
+        library = vim.api.nvim_get_runtime_file("", true),
+        checkThirdParty = false,
+      },
+      -- Do not send telemetry data containing a randomized but unique identifier
+      telemetry = {
+        enable = false,
+      },
+    },
+  },
 }
 return {
   on_setup = function(server)
