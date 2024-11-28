4f2e7f2648db83c9e124a0bd92f31ca5d5e387ca
Xin Chen <nshen121@gmail.com>
2024-11-28 13:31:21 +0800

    fix(prismals): fix prisma format

--- lua/insis/lsp/config/prismals.lua
+++ lua/insis/lsp/config/prismals.lua
@@ -1,6 +1,30 @@
 -- https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#prismals
+local util = require("lspconfig.util")
+local Path = require("plenary.path")
+
+local root_pattern_func = util.root_pattern("package.json", ".git")
+local root_dir = root_pattern_func(vim.fn.getcwd())
+local prismaPath = Path:new(root_dir, "node_modules", ".bin", "prisma"):absolute()
+
+local opts = {
+  on_attach = function(_, bufnr)
+    local lsp = require("insis").config.lsp
+    local opt = { noremap = true, silent = true, buffer = bufnr }
+    if lsp then
+      keymap("n", lsp.format, function()
+        vim.lsp.buf.format({ async = true })
+      end, opt)
+    end
+  end,
+  settings = {
+    prisma = {
+      prismaFmtBin = prismaPath,
+    },
+  },
+}
+
 return {
   on_setup = function(server)
-    server.setup({})
+    server.setup(opts)
   end,
 }
