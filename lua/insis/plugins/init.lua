82c0975f6ba1ab68c6f4763fc99d955484f39556
Xin Chen <nshen121@gmail.com>
2024-12-13 12:47:14 +0800

    feat(copilotChat.nvim): remove canary branch from CopilotChat.nvim

--- lua/insis/plugins/init.lua
+++ lua/insis/plugins/init.lua
@@ -351,7 +351,6 @@ return {
   },
   {
     "CopilotC-Nvim/CopilotChat.nvim",
-    branch = "canary",
     dependencies = {
       { "zbirenbaum/copilot.lua" }, -- or github/copilot.vim
       { "nvim-lua/plenary.nvim" }, -- for curl, log wrapper
