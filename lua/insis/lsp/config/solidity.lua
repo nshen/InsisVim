7252f288fd9dc1175f1d00aea5ab832b5863cdec
Xin Chen <nshen121@gmail.com>
2025-02-10 13:04:14 +0800

    fix(solidity): fix solidity lsp config

--- lua/insis/lsp/config/solidity.lua
+++ lua/insis/lsp/config/solidity.lua
@@ -1,14 +1,26 @@
 local common = require("insis.lsp.common-config")
-local lspconfig = require("lspconfig")
+local util = require("lspconfig.util")
+
+local root_files = {
+  "foundry.toml",
+  "remappings.txt",
+  "hardhat.config.js",
+  "hardhat.config.ts",
+  "truffle.js",
+  "truffle-config.js",
+  "ape-config.yaml",
+}
+
 local opts = {
   capabilities = common.capabilities,
   flags = common.flags,
-  on_attach = function(_, bufnr)
+  on_attach = function(client, bufnr)
     common.keyAttach(bufnr)
+    -- common.disableFormat(client)
   end,
   cmd = { "nomicfoundation-solidity-language-server", "--stdio" },
   filetypes = { "solidity" },
-  root_dir = lspconfig.util.root_pattern("foundry.toml", "hardhat.config.*", "package.json", ".git"),
+  root_dir = util.root_pattern(unpack(root_files)) or util.root_pattern(".git", "package.json"),
   single_file_support = true,
 }
 return {
