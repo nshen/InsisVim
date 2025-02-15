35222e2b303ee921bf05081c056f843f6a5fac70
Xin Chen <nshen121@gmail.com>
2025-02-15 18:17:00 +0800

    fix(codecompanion): fix aliyun siliconflow reasoning

--- lua/insis/plugins/codecompanion.lua
+++ lua/insis/plugins/codecompanion.lua
@@ -3,38 +3,6 @@ local code = pRequire("codecompanion")
 if code then
   code.setup({
     adapters = {
-      siliconflow = function()
-        return require("codecompanion.adapters").extend("openai_compatible", {
-          env = {
-            url = "https://api.siliconflow.cn", -- optional: default value is ollama url http://127.0.0.1:11434
-            api_key = function()
-              return os.getenv("DEEPSEEK_API_KEY_S")
-            end,
-            chat_url = "/v1/chat/completions", -- optional: default value, override if different
-          },
-          schema = {
-            model = {
-              default = "deepseek-ai/DeepSeek-V3",
-            },
-          },
-        })
-      end,
-      aliyun = function()
-        return require("codecompanion.adapters").extend("openai_compatible", {
-          env = {
-            url = "https://dashscope.aliyuncs.com", -- optional: default value is ollama url http://127.0.0.1:11434
-            api_key = function()
-              return os.getenv("DEEPSEEK_API_ALIYUN")
-            end,
-            chat_url = "/compatible-mode/v1/chat/completions", -- optional: default value, override if different
-          },
-          schema = {
-            model = {
-              default = "deepseek-r1",
-            },
-          },
-        })
-      end,
       deepseek = function()
         return require("codecompanion.adapters").extend("deepseek", {
           env = {
@@ -49,9 +17,47 @@ if code then
           },
         })
       end,
+      siliconflow = function()
+        return require("codecompanion.adapters").extend("deepseek", {
+          url = "https://api.siliconflow.cn/v1/chat/completions",
+          env = {
+            api_key = function()
+              return os.getenv("DEEPSEEK_API_KEY_S")
+            end,
+          },
+          schema = {
+            model = {
+              default = "deepseek-ai/DeepSeek-R1",
+              choices = {
+                ["deepseek-ai/DeepSeek-R1"] = { opts = { can_reason = true } },
+                "deepseek-ai/DeepSeek-V3",
+              },
+            },
+          },
+        })
+      end,
+      aliyun = function()
+        return require("codecompanion.adapters").extend("deepseek", {
+          url = "https://dashscope.aliyuncs.com/compatible-mode/v1/chat/completions",
+          env = {
+            api_key = function()
+              return os.getenv("DEEPSEEK_API_ALIYUN")
+            end,
+          },
+          schema = {
+            model = {
+              default = "deepseek-r1",
+              choices = {
+                ["deepseek-r1"] = { opts = { can_reason = true } },
+                "qwen-omni-turbo-latest",
+              },
+            },
+          },
+        })
+      end,
     },
     strategies = {
-      chat = { adapter = "aliyun" },
+      chat = { adapter = "siliconflow" },
       inline = { adapter = "copilot" },
     },
   })
