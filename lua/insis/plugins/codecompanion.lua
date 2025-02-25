5fed51de5efd8f27831ea35cb1e7145c5156d084
Xin Chen <nshen121@gmail.com>
2025-02-25 20:16:29 +0800

    fix(codecompanion): update for video

--- lua/insis/plugins/codecompanion.lua
+++ lua/insis/plugins/codecompanion.lua
@@ -1,113 +1,173 @@
 local code = pRequire("codecompanion")
 local cfg = require("insis").config
 
-if code then
-  code.setup({
-    adapters = {
-      deepseek = function()
-        return require("codecompanion.adapters").extend("deepseek", {
-          name = "deepseek",
-          env = {
-            api_key = function()
-              return os.getenv("DEEPSEEK_API_KEY")
-            end,
-          },
-          schema = {
-            model = {
-              default = "deepseek-coder",
-            },
-          },
-        })
-      end,
-      siliconflow = function()
-        return require("codecompanion.adapters").extend("deepseek", {
-          name = "siliconflow",
-          url = "https://api.siliconflow.cn/v1/chat/completions",
-          env = {
-            api_key = function()
-              return os.getenv("DEEPSEEK_API_KEY_S")
-            end,
-          },
-          schema = {
-            model = {
-              default = "deepseek-ai/DeepSeek-R1",
-              choices = {
-                ["deepseek-ai/DeepSeek-R1"] = { opts = { can_reason = true } },
-                "deepseek-ai/DeepSeek-V3",
-              },
-            },
-          },
-        })
-      end,
-      aliyun_deepseek = function()
-        return require("codecompanion.adapters").extend("deepseek", {
-          name = "aliyun_deepseek",
-          url = "https://dashscope.aliyuncs.com/compatible-mode/v1/chat/completions",
-          env = {
-            api_key = function()
-              return os.getenv("DEEPSEEK_API_ALIYUN")
-            end,
-          },
-          schema = {
-            model = {
-              default = "deepseek-r1",
-              choices = {
-                ["deepseek-r1"] = { opts = { can_reason = true } },
-              },
-            },
-          },
-        })
-      end,
-      -- 阿里千问
-      aliyun_qwen = function()
-        return require("codecompanion.adapters").extend("openai_compatible", {
-          name = "aliyun_qwen",
-          env = {
-            url = "https://dashscope.aliyuncs.com", -- optional: default value is ollama url http://127.0.0.1:11434
-            api_key = function()
-              return os.getenv("DEEPSEEK_API_ALIYUN")
-            end,
-            chat_url = "/compatible-mode/v1/chat/completions", -- optional: default value, override if different
-          },
-          schema = {
-            model = {
-              default = "qwen-omni-turbo-latest",
-            },
-          },
-        })
-      end,
-      opts = {
-        show_defaults = false,
-      },
-    },
-    strategies = {
-      chat = { adapter = "aliyun_qwen" },
-      inline = { adapter = "copilot" },
-    },
+if not code then
+  return
+end
+
+code.setup({
+  adapters = {
     opts = {
-      language = "Chinese",
+      -- show_defaults 会导致copilot不能正常工作
+      show_defaults = true,
+      -- log_level = "DEBUG",
     },
-    prompt_library = {
-      ["My New Prompt"] = {
-        strategy = "chat",
-        description = "Some cool custom prompt you can do",
-        prompts = {
-          {
-            role = "system",
-            content = "You are an experienced developer with Lua and Neovim",
-          },
-          {
-            role = "user",
-            content = "Can you explain why ...",
+
+    deepseek = function()
+      return require("codecompanion.adapters").extend("deepseek", {
+        name = "deepseek",
+        env = {
+          api_key = function()
+            return os.getenv("DEEPSEEK_API_KEY")
+          end,
+        },
+        schema = {
+          model = {
+            default = "deepseek-coder",
           },
         },
-      },
-    },
+      })
+    end,
+
+    siliconflow_r1 = function()
+      return require("codecompanion.adapters").extend("deepseek", {
+        name = "siliconflow_r1",
+        url = "https://api.siliconflow.cn/v1/chat/completions",
+        env = {
+          api_key = function()
+            return os.getenv("DEEPSEEK_API_KEY_S")
+          end,
+        },
+        schema = {
+          model = {
+            default = "deepseek-ai/DeepSeek-R1",
+            choices = {
+              ["deepseek-ai/DeepSeek-R1"] = { opts = { can_reason = true } },
+              "deepseek-ai/DeepSeek-V3",
+            },
+          },
+        },
+      })
+    end,
+
+    siliconflow_v3 = function()
+      return require("codecompanion.adapters").extend("deepseek", {
+        name = "siliconflow_v3",
+        url = "https://api.siliconflow.cn/v1/chat/completions",
+        env = {
+          api_key = function()
+            return os.getenv("DEEPSEEK_API_KEY_S")
+          end,
+        },
+        schema = {
+          model = {
+            default = "deepseek-ai/DeepSeek-V3",
+            choices = {
+              "deepseek-ai/DeepSeek-V3",
+              ["deepseek-ai/DeepSeek-R1"] = { opts = { can_reason = true } },
+            },
+          },
+        },
+      })
+    end,
+
+    aliyun_deepseek = function()
+      return require("codecompanion.adapters").extend("deepseek", {
+        name = "aliyun_deepseek",
+        url = "https://dashscope.aliyuncs.com/compatible-mode/v1/chat/completions",
+        env = {
+          api_key = function()
+            return os.getenv("DEEPSEEK_API_ALIYUN")
+          end,
+        },
+        schema = {
+          model = {
+            default = "deepseek-r1",
+            choices = {
+              ["deepseek-r1"] = { opts = { can_reason = true } },
+            },
+          },
+        },
+      })
+    end,
+    -- 阿里千问
+    -- https://help.aliyun.com/zh/model-studio/getting-started/models?spm=a2c4g.11186623.0.0.ce3c4823l7PTRL#9f8890ce29g5u
+    aliyun_qwen = function()
+      return require("codecompanion.adapters").extend("openai_compatible", {
+        name = "aliyun_qwen",
+        env = {
+          url = "https://dashscope.aliyuncs.com",
+          api_key = function()
+            return os.getenv("DEEPSEEK_API_ALIYUN")
+          end,
+          chat_url = "/compatible-mode/v1/chat/completions",
+        },
+        schema = {
+          model = {
+            default = "qwen-coder-plus-latest",
+          },
+        },
+      })
+    end,
+  },
+
+  strategies = {
+    chat = { adapter = "copilot" },
+    inline = { adapter = "copilot" },
+    -- copilot
+    -- choices = {
+    --   ["o3-mini-2025-01-31"] = { opts = { can_reason = true } },
+    --   ["o1-2024-12-17"] = { opts = { can_reason = true } },
+    --   ["o1-mini-2024-09-12"] = { opts = { can_reason = true } },
+    --   "claude-3.5-sonnet",
+    --   "gpt-4o-2024-08-06",
+    --   "gemini-2.0-flash-001",
+    -- },
+  },
+
+  opts = {
+    language = "Chinese",
+  },
+  -------------------------------------------
+  prompt_library = {
+    ["DeepSeek Explain In Chinese"] = require("insis.utils.promps.deepseek-explain"),
+    ["Nextjs Helper"] = require("insis.utils.promps.nextjs-helper"),
+  },
+})
+
+keymap({ "n", "v", "x" }, cfg.codecompanion.keys.toggle_chat, function()
+  code.toggle()
+end)
+
+keymap({ "n", "v", "x" }, cfg.codecompanion.keys.prompt_actions, ":CodeCompanionActions<CR>")
+
+-- https://github.com/olimorris/codecompanion.nvim/discussions/640
+local fidget = pRequire("fidget")
+local handler
+if fidget then
+  -- Attach:
+  vim.api.nvim_create_autocmd({ "User" }, {
+    pattern = "CodeCompanionRequest*",
+    group = vim.api.nvim_create_augroup("CodeCompanionHooks", {}),
+    callback = function(request)
+      if request.match == "CodeCompanionRequestStarted" then
+        if handler then
+          handler.message = "Abort."
+          handler:cancel()
+          handler = nil
+        end
+        handler = fidget.progress.handle.create({
+          title = "",
+          message = "Thinking...",
+          lsp_client = { name = "CodeCompanion" },
+        })
+      elseif request.match == "CodeCompanionRequestFinished" then
+        if handler then
+          handler.message = "Done."
+          handler:finish()
+          handler = nil
+        end
+      end
+    end,
   })
-
-  keymap({ "n", "v", "x" }, cfg.codecompanion.keys.toggle_chat, function()
-    code.toggle()
-  end)
-
-  keymap({ "n", "v", "x" }, cfg.codecompanion.keys.prompt_actions, ":CodeCompanionActions<CR>")
 end
---
+++ lua/insis/utils/promps/deepseek-explain.lua
@@ -0,0 +1,52 @@
+return {
+  strategy = "chat",
+  description = "中文解释代码",
+  opts = {
+    is_slash_cmd = false,
+    modes = { "v" },
+    short_name = "explain in chinese",
+    auto_submit = true,
+    user_prompt = false,
+    stop_context_insertion = true,
+    adapter = {
+      name = "aliyun_deepseek",
+      model = "deepseek-r1",
+    },
+  },
+  prompts = {
+    {
+      role = "system",
+      content = [[当被要求解释代码时，请遵循以下步骤：
+
+  1. 识别编程语言。
+  2. 描述代码的目的，并引用该编程语言的核心概念。
+  3. 解释每个函数或重要的代码块，包括参数和返回值。
+  4. 突出说明使用的任何特定函数或方法及其作用。
+  5. 如果适用，提供该代码如何融入更大应用程序的上下文。]],
+      opts = {
+        visible = false,
+      },
+    },
+    {
+      role = "user",
+      content = function(context)
+        local input = require("codecompanion.helpers.actions").get_code(context.start_line, context.end_line)
+
+        return string.format(
+          [[请解释 buffer %d 中的这段代码:
+
+  ```%s
+  %s
+  ```
+  ]],
+          context.bufnr,
+          context.filetype,
+          input
+        )
+      end,
+      opts = {
+        contains_code = true,
+      },
+    },
+  },
+}
